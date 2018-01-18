0907f47fe4f8f8c58459ba2954b424d30be5503a
cld378632668 <378632668@qq.com>
2018-01-18 12:06:11 +0800

    update

--- requests_introduce.py
+++ requests_introduce.py
@@ -7,5 +7,5 @@ print("Response.content:"+str(r.content))
 print("Response.cookies:"+str(r.cookies))
 print("Response.headers:"+str(r.headers))
 print("Response.history:"+str(r.history))
-
+print("Response.text:"+str(r.text))
 print("occupy for debug ")
